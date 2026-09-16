451ef93b212a734612b9e52ce9eca8251bc8a628
Willis1P <willisiqoption@hotmail.com>
2026-09-16 16:58:23 -0300

    chore(sw): bump v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lotofacil-v10';
+const CACHE_NAME = 'lotofacil-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png', './lotofacil-capa-2.webp'];
 
 self.addEventListener('install', (e) => {
